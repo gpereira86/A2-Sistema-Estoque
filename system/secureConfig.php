4630dfa48e850997c42a1c7bc9492605ee5dcf34
Glauco Pereira <glaucoadm@gmail.com>
2025-04-25 14:05:30 -0300

    Removendo arquivos ignorados

--- system/secureConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Informações seguras → variáveis globais que não serão enviadas para o repositório remoto.
- *
- * obs.: criado para incluir dados de autenticação em Banco ao subir aplicação no meu host pessoal.
- */
