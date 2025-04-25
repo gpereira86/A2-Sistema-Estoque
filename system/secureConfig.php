9d0a622763ed5a056f429c828fa955a5dc71d68a
Glauco Pereira <glaucoadm@gmail.com>
2025-04-25 14:05:12 -0300

    comiit

---
+++ system/secureConfig.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Informações seguras → variáveis globais que não serão enviadas para o repositório remoto.
+ *
+ * obs.: criado para incluir dados de autenticação em Banco ao subir aplicação no meu host pessoal.
+ */
